29e570f3de9595f916034e58c0c05f58182a6fd1
Karen N. Black <62806857+karenblack@users.noreply.github.com>
2020-11-18 14:46:52 -0600

    Delete portfolio.js

--- portfolio.js
+++
@@ -1,7 +0,0 @@
-
-// function to have pop-up alert after submitting the form data
-window.onload = function() {
-	document.getElementById("form").onsubmit = function() {
-		alert("Thank you for your submission!");
-	}
-}
